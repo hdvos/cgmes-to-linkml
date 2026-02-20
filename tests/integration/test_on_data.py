d39ac80e61663c4c71cd4e51b94f392d120e8636
hdvos <hugo.de.vos@alliander.com>
2026-02-20 11:42:26 +0100

    Add some spacing to aid readablity.

--- tests/integration/test_on_data.py
+++ tests/integration/test_on_data.py
@@ -19,12 +19,14 @@ def test_on_data(data_file):
     
     with tempfile.NamedTemporaryFile(delete=False) as temp_f:
         temp_file_path = Path(temp_f.name)
+        
     try:
         # CLI is expected to exit with SystemExit, so catch it to prevent the test from failing.
         with pytest.raises(SystemExit):
             cli.main([str(data_file), "-o", str(temp_file_path)])
             
         assert temp_file_path.exists()
+        
     finally:
         # Cleanup the temporary file after the test
         temp_file_path.unlink(missing_ok=True)
